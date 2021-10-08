b9ae55a7f781994ab4a493db8d6c6691a8b63ffd
Ken <okalang.ds@gmail.com>
2021-10-08 11:45:59 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityFarmActivities.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ActivityName> TodoItems { get; set; }
+    }
+}
